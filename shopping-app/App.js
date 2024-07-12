7eb4e190a8c2db8e92e8b4d9e090f6b8630d4b14
nanagyanfosu <nanagyanfosu@gmail.com>
2024-07-12 12:34:47 +0000

    added images to assets folder

--- shopping-app/App.js
+++ shopping-app/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './Homepage';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { CartProvider } from './home/CartContext';
+import Cart from './Cart';
+import ProductScreen from './ProductScreen';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+// function Root(){
+//   return(
+//     <NavigationContainer>
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Store" /> 
+//       <Drawer.Screen name="Locations" /> 
+//       <Drawer.Screen name="Blog"/> 
+//       <Drawer.Screen name="Jewelry"/> 
+//       <Drawer.Screen name="Electronic"/> 
+//       <Drawer.Screen name="Clothing"/> 
+//     </Drawer.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <CartProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Homepage} options={{headerShown: false}}/>
+      <Stack.Screen name="Cart" component={Cart} options={{headerShown: false}} />
+      <Stack.Screen name="ProductScreen" component={ProductScreen} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </CartProvider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
